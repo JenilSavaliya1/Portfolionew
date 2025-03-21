d4e55af64a3a6b29845643d07ae232e40cf3a117
jenil <jn569163@dal.ca>
2025-03-21 15:45:27 -0300

    trying to fetch the projects

--- netlify/functions/projects.js
+++ netlify/functions/projects.js
@@ -1,8 +1,12 @@
 const fs = require('fs').promises; 
+const path = require('path');
 
 exports.handler = async function(event, context) {
+    const filePath = path.join(__dirname, '..', '..', 'projects.json');
+    console.log(filePath);
     try {
-        const data = await fs.readFile('./projects.json', 'utf8'); 
+        const data = await fs.readFile(filePath, 'utf8'); 
+        console.log(data);
         return {
             statusCode: 200,
             body: data
